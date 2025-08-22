51a98055e37c87569c5fb13a67bea54356101ee5
Daniel <danielandresogodieme@gmail.com>
2025-08-22 15:31:55 +0000

    feat: toggle form visibility

--- src/components/NoteForm.jsx
+++ src/components/NoteForm.jsx
@@ -7,6 +7,8 @@ const NoteForm = ({ notes, setNotes }) => {
     desc: "",
   });
 
+  const [isFormVisible, setIsFormVisible] = useState(false);
+
   const handlechange = (e) => {
     setFormData({
       ...formData,
@@ -36,83 +38,95 @@ const NoteForm = ({ notes, setNotes }) => {
   };
 
   return (
-    <form className="mb-6" onSubmit={onSubmit}>
-      {/* Title */}
-      <div className="mb-4">
-        <label htmlFor="title" className="block font-semibold">
-          Title
-        </label>
-        <input
-          type="text"
-          name="title"
-          id="title"
-          className="w-full p-2 border rounded-lg"
-          value={formData.title}
-          onChange={handlechange}
-        />
-      </div>
+    <>
+      {/* Toggle form visibility button */}
+      <button
+        className="w-full bg-gray-100 border-purple-300 border text-purple-800 py-2 rounded-lg cursor-pointer hover:bg-purple-300 hover:border-purple-300 transition mb-4"
+        onClick={() => setIsFormVisible(!isFormVisible)}
+      >
+        {isFormVisible ? "Hide form ✖️" : "Add New Note ➕"}
+      </button>
+      {/* Form */}
+      {isFormVisible && (
+        <form className="mb-6" onSubmit={onSubmit}>
+          {/* Title */}
+          <div className="mb-4">
+            <label htmlFor="title" className="block font-semibold">
+              Title
+            </label>
+            <input
+              type="text"
+              name="title"
+              id="title"
+              className="w-full p-2 border rounded-lg"
+              value={formData.title}
+              onChange={handlechange}
+            />
+          </div>
 
-      {/* Priority */}
-      <div className="mb-4">
-        <label htmlFor="prio" className="block font-semibold">
-          Priority
-        </label>
-        <select
-          type="text"
-          name="prio"
-          id="prio"
-          className="w-full p-2 border rounded-lg"
-          value={formData.prio}
-          onChange={handlechange}
-        >
-          <option value="High">🔴 High</option>
-          <option value="Medium">🟡 Medium</option>
-          <option value="Low">🟢 Low</option>
-        </select>
-      </div>
+          {/* Priority */}
+          <div className="mb-4">
+            <label htmlFor="prio" className="block font-semibold">
+              Priority
+            </label>
+            <select
+              type="text"
+              name="prio"
+              id="prio"
+              className="w-full p-2 border rounded-lg"
+              value={formData.prio}
+              onChange={handlechange}
+            >
+              <option value="High">🔴 High</option>
+              <option value="Medium">🟡 Medium</option>
+              <option value="Low">🟢 Low</option>
+            </select>
+          </div>
 
-      {/* Category */}
-      <div className="mb-4">
-        <label htmlFor="category" className="block font-semibold">
-          Category
-        </label>
-        <select
-          type="text"
-          name="category"
-          id="category"
-          className="w-full p-2 border rounded-lg"
-          value={formData.category}
-          onChange={handlechange}
-        >
-          <option value="Work">🏢 Work</option>
-          <option value="Personnal">🏠 Personnal</option>
-          <option value="Ideas">💡 Ideas</option>
-        </select>
-      </div>
+          {/* Category */}
+          <div className="mb-4">
+            <label htmlFor="category" className="block font-semibold">
+              Category
+            </label>
+            <select
+              type="text"
+              name="category"
+              id="category"
+              className="w-full p-2 border rounded-lg"
+              value={formData.category}
+              onChange={handlechange}
+            >
+              <option value="Work">🏢 Work</option>
+              <option value="Personnal">🏠 Personnal</option>
+              <option value="Ideas">💡 Ideas</option>
+            </select>
+          </div>
 
-      {/* Description */}
-      <div className="mb-4">
-        <label htmlFor="desc" className="block font-semibold">
-          Description
-        </label>
-        <textarea
-          name="desc"
-          id="desc"
-          value={formData.desc}
-          className="w-full p-2 border rounded-lg"
-          onChange={handlechange}
-        ></textarea>
-      </div>
+          {/* Description */}
+          <div className="mb-4">
+            <label htmlFor="desc" className="block font-semibold">
+              Description
+            </label>
+            <textarea
+              name="desc"
+              id="desc"
+              value={formData.desc}
+              className="w-full p-2 border rounded-lg"
+              onChange={handlechange}
+            ></textarea>
+          </div>
 
-      {/* Submit button */}
-      <div className="mb-4">
-        <input
-          type="submit"
-          value="Add note"
-          className="bg-purple-600 w-full p-2 border rounded-lg text-white font-semibold cursor-pointer active:bg-purple-400"
-        />
-      </div>
-    </form>
+          {/* Submit button */}
+          <div className="mb-4">
+            <input
+              type="submit"
+              value="Add note"
+              className="bg-purple-600 w-full p-2 border rounded-lg text-white font-semibold cursor-pointer active:bg-purple-400"
+            />
+          </div>
+        </form>
+      )}
+    </>
   );
 };
 
